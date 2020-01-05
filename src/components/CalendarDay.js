a614e2f7d039ff3599df83fe879c153657d23667
stupendousC <CarolineWuKaplan@gmail.com>
2020-01-04 22:13:31 -0800

    will show all shifts depending on day, ugly table, BUG: will not load curr day automatically, must think of a way to make this the LAST step of componentDidMount(), AFTER loading from db

--- src/components/CalendarDay.js
+++ src/components/CalendarDay.js
@@ -3,77 +3,53 @@ import React from 'react';
 const CalendarDay = ({completeShiftsInfo, dateStr}) => {
   console.log("CalendarDay received date=", dateStr);
 
-  // completeShiftsInfo = [ [shift1], [shift2], [shift3], etc ]
-    // subarray = [[shiftInfo], [employeeInfo], [clientInfo]] 
-
-  console.log(completeShiftsInfo);
-  for (const eachShift of completeShiftsInfo) {
-    console.log(eachShift[0].id);
-    console.log(eachShift[1].name);
-    console.log(eachShift[2]);
-  }
+  // completeShiftsInfo = [ [shift1], [shift2], etc ]
+    // triplet subarray = [[shiftInfo], [employeeInfo], [clientInfo]] 
 
   const showShifts = () => {
-    if (completeShiftsInfo.length === 0) {
-      return(
-        <h3>No shifts scheduled
-        <br />BUG!!! today doesn't show automatically, assumed none!</h3>
-      );
-    } else {
-
-      return ( completeShiftsInfo.map (shift => {
-        return (
-        <tr key={shift.id}> 
-          <td>{shift[1].name}</td>
-          <td>{shift[2].name}</td>
-          <td><button className="btn btn-primary">Update</button></td>
-          <td><button className="btn btn-primary">Delete</button></td>
+    return ( completeShiftsInfo.map (tripletArray => {
+      return (
+        <tr key={tripletArray.id} className="table-4-col"> 
+          <td>{tripletArray[1].name}</td>
+          <td>{tripletArray[2].name}</td>
+          <td>{tripletArray[0].start_time}</td>
+          <td>{tripletArray[0].end_time}</td>
         </tr>
         );
     }));
-    }    
   }
-  
-  // <td>shift.id = {shift.id}</td>
-  //       <td>shift.client_id = {shift.client_id}</td>
-  //       <td>shift.employee_id = {shift.employee_id}</td>
-  //       <td>shift.shift_date = {shift.shift_date}</td>
-  //       <td>shift.start_time = {shift.start_time}</td>
-  //       <td>shift.end_time = {shift.end_time}</td>
-  
-  
-  // showAllShifts = () => {
-  //   const URL_endpoint = BASE_URL + ALL_SHIFTS
-  //   const listFromState = this.state.allShifts
-  //   return ( listFromState.map((shift, i) => {
-  //     return (
-  //       <section>
-  //         <tr key={i}>
-  //           <td>{shift.shift_date}</td>
-  //           <td><button onClick={() => this.read(i, listFromState)} className="btn btn-primary">Info</button></td>
-  //           <td><button onClick={() => this.update(i, listFromState)} className="btn btn-primary">TODO: Update</button></td>
-  //           <td><button onClick={() => this.delete(shift, URL_endpoint)} className="btn btn-danger">Delete</button></td>
-  //         </tr>
-  //         <tr>
-  //           {this.state.personSpotlight === shift ? this.showPersonSpotlight(shift):null}
-  //         </tr>
-  //       </section>
-  //     )})
-  //   );
-  // }
 
+  const showTableOrNothing = () => {
+    if (completeShiftsInfo.length === 0) {
+      return (
+        <h3>No shifts scheduled</h3>
+      );
+    } else {
+      return (
+        <table>
+          <thead className="table-4-col"> 
+            <td>Client</td>
+            <td>Employee</td>
+            <td>Start</td>
+            <td>End</td>
+          </thead>
 
-
+          <tbody>
+            {showShifts()}
+          </tbody>
+        </table>
+      );
+    }
+  }
 
   return(
     <section>
       <h1>All Shifts For {dateStr}</h1>
-      {showShifts()}
+      {showTableOrNothing()}
     </section>
   );
   
 }
 
 export default CalendarDay;
-
 
