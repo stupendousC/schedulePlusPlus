c71937655bdc8d7f4d14afe796920bcd000e6aa2
stupendousC <CarolineWuKaplan@gmail.com>
2020-01-09 15:12:10 -0800

    accordian of full shift info successfully implemented, pretty it up later

--- src/components/AdminDash_ShiftsTable.js
+++ src/components/AdminDash_ShiftsTable.js
@@ -12,6 +12,7 @@ const EmployeeDash_ShiftsTable = ({allShifts}) => {
     if (shift.employee) {
       return (shift.employee.name);
     } else {
+      // return ("argh");
       return (<button onClick={()=>getAvailEmps(shift)} className="btn btn-success">Find employees</button>);
     }
   }
@@ -30,6 +31,44 @@ const EmployeeDash_ShiftsTable = ({allShifts}) => {
     //
   }
 
+  const showShiftInfoCard = (shift) => {
+    console.log(shift);
+    return (
+      <card>
+        <section className="card-shift">
+          <p>DATE</p>
+          <p>{shift.shift_date}</p>
+          <p>START</p>
+          <p>{convertTimeString(shift.start_time)}</p>
+          <p>END</p>
+          <p>{convertTimeString(shift.start_time)}</p>
+        </section>
+
+        <section className="card-employee">
+          <p>EMPLOYEE</p>
+          { shift.employee ? <p>{shift.employee.name}</p> : <p></p> }
+          <p>PHONE</p>
+          { shift.employee ? <p>{shift.employee.phone}</p> : <p></p> }
+          <p>EMAIL</p>
+          { shift.employee ? <p>{shift.employee.email}</p> : <p></p> }
+          <p>ADDRESS</p>
+          { shift.employee ? <p>{shift.employee.address}</p> : <p></p> }
+        </section>
+
+        <section className="card-client">
+          <p>CLIENT</p>
+          { shift.client ? <p>{shift.client.name}</p> : <p></p> }
+          <p>PHONE</p>
+          { shift.client ? <p>{shift.client.phone}</p> : <p></p> }
+          <p>EMAIL</p>
+          { shift.client ? <p>{shift.client.email}</p> : <p></p> }
+          <p>ADDRESS</p>
+          { shift.client ? <p>{shift.client.address}</p> : <p></p> }
+        </section>
+      </card>
+    );
+
+  }
 
   ////////////////// render ////////////////////
   if (allShifts.length === 0) {
@@ -40,110 +79,56 @@ const EmployeeDash_ShiftsTable = ({allShifts}) => {
   } else {
     return(
       <section>
-        <h1>PLANS</h1>
-        <li>staff it! button also shows card under row with who's around</li>
-        <li>collapsible menu for more info</li>
-        <li>collapsible menu for those in the past</li>
-
-        <Accordion defaultActiveKey="0">
-  <Card>
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="0">
-        Click me!
-      </Accordion.Toggle>
-    </Card.Header>
-    <Accordion.Collapse eventKey="0">
-      <Card.Body>Hello! I'm the body</Card.Body>
-    </Accordion.Collapse>
-  </Card>
-  <Card>
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="1">
-        Click me!
-      </Accordion.Toggle>
-    </Card.Header>
-    <Accordion.Collapse eventKey="1">
-      <Card.Body>Hello! I'm another body</Card.Body>
-    </Accordion.Collapse>
-  </Card>
-</Accordion>
-
-
 
         {allShifts.map(shift => {
           return (
-            <section key = {shift.id} className="section-4-col">
-              <section>{formatDate(shift.shift_date)}</section>
-              <section>{shift.client.name}</section>
-              <section>{showEmpNameOrButton(shift)}</section>
-              <section>{convertTimeString(shift.start_time)} to {convertTimeString(shift.end_time)}</section>
-            </section>
+            <Accordion key={shift.id}>
+              <section>
+                <Accordion.Toggle eventKey="showInfo" className="accordian-toggle_button">
+                  <section className="section-4-col">
+                    <section>Info</section>
+                    <section>{formatDate(shift.shift_date)}</section>
+                    <section>{shift.client.name}</section>
+                    <section>{showEmpNameOrButton(shift)}</section>
+                  </section>
+                </Accordion.Toggle>
+
+                <Accordion.Collapse eventKey="showInfo">
+                  <section>{showShiftInfoCard(shift)}</section>
+                </Accordion.Collapse>
+
+              </section>
+            </Accordion>
+
           )}
+
+            
+            
         )}
       </section>
     );
   }    
-
-
-
-
-
-
-
-
-
-
-// return(
-//   <section>
-//     NOTHING YET
-//     <div id="accordion">
-//   <div className="card">
-//     <div className="card-header" id="headingOne">
-//       <h5 className="mb-0">
-//         <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-//           Collapsible Group Item #1
-//         </button>
-//       </h5>
-//     </div>
-
-//     <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-//       <div className="card-body">
-//         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-//       </div>
-//     </div>
-//   </div>
-//   <div className="card">
-//     <div className="card-header" id="headingTwo">
-//       <h5 className="mb-0">
-//         <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-//           Collapsible Group Item #2
-//         </button>
-//       </h5>
-//     </div>
-//     <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-//       <div className="card-body">
-//         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-//       </div>
-//     </div>
-//   </div>
-//   <div className="card">
-//     <div className="card-header" id="headingThree">
-//       <h5 className="mb-0">
-//         <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-//           Collapsible Group Item #3
-//         </button>
-//       </h5>
-//     </div>
-//     <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-//       <div className="card-body">
-//         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-//       </div>
-//     </div>
-//   </div>
-// </div>
-//   </section>
-// );
-
 }
 
 export default EmployeeDash_ShiftsTable;
+
+
+
+// const showAccordion = () => {
+//   return (
+//     <Accordion>
+//         <section>
+          
+//           <section>
+//             <Accordion.Toggle eventKey="0">Info</Accordion.Toggle>
+//           </section>
+
+//           <Accordion.Collapse eventKey="0">
+//             <section>Show entire shift info here</section>
+//           </Accordion.Collapse>
+
+//         </section>
+
+//       </Accordion>
+//   );
+// }
