cb6a87bae9003b00091917c972b6e78e3c6efb01
stupendousC <CarolineWuKaplan@gmail.com>
2020-01-09 12:17:35 -0800

    attempting showing availableEmployees via collapsible menu

--- src/components/AdminDash_ShiftsTable.js
+++ src/components/AdminDash_ShiftsTable.js
@@ -16,19 +16,21 @@ const EmployeeDash_ShiftsTable = ({allShifts}) => {
   }
 
   const getAvailEmps = (shift) => {
-    console.log("ok, gonna find soemone...", shift.shift_date, "for shift obj", shift);
     
-    const URL = process.env.REACT_APP_GET_AVAIL_EMPS;
-    axios.get(URL+`/${shift.id}`)
+    const URL = process.env.REACT_APP_GET_AVAIL_EMPS + `/${shift.id}`;
+    console.log("ok, gonna find soemone for shift obj", shift);
+
+    axios.get(URL)
     .then(response => {
-      console.log("backend says: ", response.data);
+      console.log("backend returns list of avail emps: ", response.data);
+      // 
     })
     .catch(error => console.log("Error getting availEmps:", error.message));
-    // 
+    //
   }
 
 
-
+  ////////////////// render ////////////////////
   if (allShifts.length === 0) {
     return (
       <section>No upcoming shifts</section>
@@ -37,9 +39,43 @@ const EmployeeDash_ShiftsTable = ({allShifts}) => {
   } else {
     return(
       <section>
-        <h1>Would be nice to click on row for more info</h1>
-        <li>show red if not staffed yet</li>
-        <li>grey out or hide those in the past</li>
+        <h1>PLANS</h1>
+        <li>staff it! button also shows card under row with who's around</li>
+        <li>collapsible menu for more info</li>
+        <li>collapsible menu for those in the past</li>
+
+        <div id="accordion">
+  <div className="card">
+    <div className="card-header">
+        <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" />
+          Collapsible Group Item #1
+        
+    </div>
+
+    <div id="collapseOne" className="collapse show"data-parent="#accordion">
+      <div className="card-body">
+        ok...
+      </div>
+    </div>
+  </div>
+
+  <div className="card">
+    <div className="card-header" id="headingTwo">
+        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" />
+          Collapsible Group Item #2
+        
+    </div>
+
+    <div id="collapseTwo" className="collapse" data-parent="#accordion">
+      <div className="card-body">
+        hi there
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
 
         {allShifts.map(shift => {
           return (
@@ -54,6 +90,67 @@ const EmployeeDash_ShiftsTable = ({allShifts}) => {
       </section>
     );
   }    
+
+
+
+
+
+
+
+
+
+
+// return(
+//   <section>
+//     NOTHING YET
+//     <div id="accordion">
+//   <div className="card">
+//     <div className="card-header" id="headingOne">
+//       <h5 className="mb-0">
+//         <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+//           Collapsible Group Item #1
+//         </button>
+//       </h5>
+//     </div>
+
+//     <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+//       <div className="card-body">
+//         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+//       </div>
+//     </div>
+//   </div>
+//   <div className="card">
+//     <div className="card-header" id="headingTwo">
+//       <h5 className="mb-0">
+//         <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+//           Collapsible Group Item #2
+//         </button>
+//       </h5>
+//     </div>
+//     <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+//       <div className="card-body">
+//         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+//       </div>
+//     </div>
+//   </div>
+//   <div className="card">
+//     <div className="card-header" id="headingThree">
+//       <h5 className="mb-0">
+//         <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+//           Collapsible Group Item #3
+//         </button>
+//       </h5>
+//     </div>
+//     <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+//       <div className="card-body">
+//         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+//       </div>
+//     </div>
+//   </div>
+// </div>
+//   </section>
+// );
+
 }
 
 export default EmployeeDash_ShiftsTable;
