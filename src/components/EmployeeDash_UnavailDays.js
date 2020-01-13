1a5cc22d6320e870c12212808b8c263a803ea98b
stupendousC <CarolineWuKaplan@gmail.com>
2020-01-12 23:12:09 -0800

    added css to center some texts

--- src/components/EmployeeDash_UnavailDays.js
+++ src/components/EmployeeDash_UnavailDays.js
@@ -6,7 +6,7 @@ const UnavailDays = ({sortedUnavails, freeToWorkCallback}) => {
   const showUnavailsTable = () => {
     return sortedUnavails.map(unavail => {
       return(
-        <section key={unavail.id} className="section-2-col">
+        <section key={unavail.id} className="employee-dash-unavails-table">
           <section>{formatDate(unavail.day_off)}</section>
           <button onClick={()=>{freeToWorkCallback(unavail)}} className="btn btn-success">I'm free to work</button>
         </section>
@@ -16,7 +16,7 @@ const UnavailDays = ({sortedUnavails, freeToWorkCallback}) => {
 
   return(
     <section> 
-      <h1>UNAVAILABLE DAYS</h1>
+      <h1 className="text-centered">UNAVAILABLE DAYS</h1>
       {showUnavailsTable()}
     </section>
   );
--- src/components/LoginError.js
+++ src/components/LoginError.js
@@ -5,7 +5,7 @@ export default function LoginError(props) {
 
   return (
     <section>
-      <h1>{props.message}</h1>
+      <h1 className="text-centered">{props.message}</h1>
       <img src={blackLock} alt="blackLock" className="homepage-logo img-90"/>
     </section>
   );
