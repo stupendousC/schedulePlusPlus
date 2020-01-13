180fd1db71175679a99b9d1d6f109149af3343ac
stupendousC <CarolineWuKaplan@gmail.com>
2020-01-12 23:03:31 -0800

    decided against moving out Calendar Tab related code into <CalendarTab> component, it got too messy for some reason, and without any real benefit either

---
+++ src/components/EmployeeDash_CalendarTab.js
@@ -0,0 +1,2 @@
+// Actually, I'm not going to outsource the CalendarTab out from EmployeeDash, I'm leaving it there for now.
+// Don't really see huge benefit of compartmentalizing employeeDash further into diff components
