b94010ee11f0eee85bd3cabbe40678782b4ff98e
stupendousC <CarolineWuKaplan@gmail.com>
2020-01-14 00:30:13 -0800

    made notes on logic flow for dealing with employees clicking on links we texted them

--- src/components/Employee_TextedLink.js
+++ src/components/Employee_TextedLink.js
@@ -2,61 +2,27 @@ import React from 'react';
 import LoginError from './LoginError';
 
 export default function LinkTextedToEmployee({match}) {
-  // NO NEED TO LOG IN, the UUID in the URL is verification enough b/c it's user's phone
-  // Just click yes or no
-
-  
     // send API call to get the shift info, then display it here
     // once they click on accept or decline button, send info back to API
     // what if they decline?  gonna have to rejigger that too... it'd be useful to know if there's a hard NO from everyone
     // but i dont want them to decline, I want to keep bugging them until someone takes the shift
     return (
-      <section className="homepage-section text-centered">
+      <section className="homepage-section">
     NO NEED TO LOG IN, the UUID in the URL is verification enough b/c it's user's phone<br/>
-    Just click yes or no<br/><br/>
+    you'll either see... 1. Confirmation screen, or 2. Sorry shift is taken<br/><br/>
 
     We received UUID = {match.params.uuid}<br/>
+
+    <h1>LOGIC FLOW</h1>
+    1. client clicking on this link, will send api call to backend and check to see if 
+    shift obj still exists in Texts db. <br/><br/>
+    2A. If yes, then see confirmation screen <br/>
+      ...2A1. Once emp clicks on confirm button, send API call to backend,<br/>
+      ...2A2. add that emp obj to the shift obj,<br/>
+      ...2A3. delete all rows in Texts that belong to that shift obj<br/><br/>
+    2B. If no, then see shift taken screen <br />
   </section>
     );
   
   
 }
-
-
-
-////////// 1st try, but this has problems with auto-redirect to employeeDash, so this pg will never display
-// export default function LinkTextedToEmployee({match}) {
-
-//     const sessionRole = sessionStorage.getItem('authenticatedRole');
-//     console.log("sessionRole = ", sessionRole);
-
-//     if (sessionRole === "ADMIN") {
-//       return <LoginError message="This is meant for employee use" />
-    
-//     } else if (sessionRole !== "EMPLOYEE") {
-//       return <LoginError message="Please log in first" />;
-
-//     } else if (match.params.id !== sessionStorage.getItem('databaseId')){
-//       return <LoginError message="Sorry, this page belongs to a different employee"/>;
- 
-//     } else {
-//       // user = correct employee
-
-//       // send API call to get the shift info, then display it here
-//       // once they click on accept or decline button, send info back to API
-//       // what if they decline?  gonna have to rejigger that too... it'd be useful to know if there's a hard NO from everyone
-//       // but i dont want them to decline, I want to keep bugging them until someone takes the shift
-//       return (
-//         <section className="homepage-section text-centered">
-//       This should be the texted link to the employee<br/><br/>
-
-//       We received id = {match.params.id}<br/>
-//       ShiftId={match.params.shiftId}<br/>
-//     </section>
-//       );
-    
-    
-//   }
-  
-
-// }
