ceb676aaf03a82c5872db1d29f04ff4625edf889
stupendousC <CarolineWuKaplan@gmail.com>
2020-01-13 20:15:17 -0800

    centered homepage pic

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,7 +4,7 @@ import twoPeople from '../images/twopeople.svg';
 export default function Homepage() {
 
   return (
-    <section className="homepage-section">
+    <section className="homepage-section text-centered">
       <img src={twoPeople} alt="2 people" className="homepage-logo"/>
     </section>
   );
