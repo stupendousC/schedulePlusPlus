2835878b731fea107d5e63e87a9ff9eeb58ab922
stupendousC <CarolineWuKaplan@gmail.com>
2020-01-18 16:17:07 -0800

    working on CRUD for all peoples on adminDash

--- src/components/AdminDash_PeopleTable.js
+++ src/components/AdminDash_PeopleTable.js
@@ -7,14 +7,15 @@ import ToastUndo from './ToastUndo';
 ///////////////////// People can be either admins, employees, or clients /////////////////////
 const PeopleTable = ({title, peopleList, URL_endpoint, setStateKey, updatePeopleListCB }) => {
   const [personSpotlight, setPersonSpotlight] = useState("");
-  
-  // not gonna useState on this b/c that's asynch AND I don't need re-rendering for it
+  const [updateSpotlightBool, setUpdateSpotlightBool] = useState(false);
+
+  // not gonna useState on the following b/c that's asynch AND I don't need re-rendering for it
   let personInPurgatory = null;
 
   const showAll = (peopleList, URL_endpoint) => {
     return ( peopleList.map((person, i) => {
       return (
-        <section key={i}>
+        <section key={i} className="margin-all-1rem">
           <section className="peopleTable">
             <section>{person.name}</section>
             <section><button onClick={() => read(i, peopleList)} className="btn btn-primary">Info</button></section>
@@ -30,16 +31,43 @@ const PeopleTable = ({title, peopleList, URL_endpoint, setStateKey, updatePeople
   }
 
   const showPersonSpotlight = (person) => {
-    return (
-      <ul>
-        <li>ID: {person.id}</li>
-        <li>OAuthId:{person.oauthid}</li>
-        <li>Name: {person.name}</li>
-        <li>Address: {person.address}</li>
-        <li>Phone: {person.phone}</li>
-        <li>Email: {person.email}</li>
-      </ul>
+    if (updateSpotlightBool === true) {
+      return (
+        <form className="margin-all-1rem">
+          <fieldset>
+            <div className="form-group">
+              <label>NAME</label>
+              <input type="text" className="form-control" placeholder={person.name}/>
+              <label>Address</label>
+              <input type="text" className="form-control" placeholder={person.address}/>
+              <label>Phone</label>
+              <input type="text" className="form-control" placeholder={person.phone}/>
+              <label>Email</label>
+              <input type="text" className="form-control" placeholder={person.email}/>
+            </div>
+            <button onClick={sendUpdateAPI} className="btn btn-primary">READ ONLY FOR NOW</button>
+          </fieldset>
+        </form>
+      );
+    } else { 
+      // read only
+      return (
+        <section className={`card-person lightblue-bg`}>
+          <p>ID</p>
+          <p>{person.id}</p> 
+          <p>NAME</p>
+          <p>{person.name}</p> 
+          <p>PHONE</p>
+          <p>{person.phone}</p> 
+          <p>EMAIL</p>
+          <p>{person.email}</p> 
+          <p>ADDRESS</p>
+          <p>{person.address}</p> 
+          <p>UUID (for 1st time login)</p>
+          <p>{person.uuid}</p> 
+        </section>
     );
+    }
   }
   ////////////////////// read/update/deactivate //////////////////////
   const toggleAsPersonSpotlight = (selectedPerson) => {
@@ -52,20 +80,34 @@ const PeopleTable = ({title, peopleList, URL_endpoint, setStateKey, updatePeople
 
   const read = (i, peopleList) => {
     const selectedPerson = peopleList[i];
+    setUpdateSpotlightBool(false);
     toggleAsPersonSpotlight(selectedPerson);
   }
 
   const update = (i, peopleList) => {
     const selectedPerson = peopleList[i];
+    setUpdateSpotlightBool(true);
     toggleAsPersonSpotlight(selectedPerson);
-    // TODO: add fields for input
+
+
+    // updatePeopleListCB(setStateKey, updatedPeopleList);
+  }
+
+  const sendUpdateAPI = (e) => {
+    e.preventDefault();
+
+    console.log("what do we have...?", e.target);
+
+    // axios.get(`${URL_endpoint}/${id}`)
+    // .then(respone => console.log('update from backend:', response.data))
+    // .catch(error => toast.error(`ERROR: ${error.message}`));
   }
 
   const deactivate = (person) => {
     setPersonSpotlight("");
     personInPurgatory = person;
 
-    console.log("TODO: If employee, need to reopen shifts they alreay committed to!!! and inform employees!!!");
+    console.log("\n\nTODO: If employee, need to reopen shifts they alreay committed to!!! and inform employees!!!");
     console.log("TODO: If client, need to close shifts they alreay committed to!!! and inform employees!!!");
 
     toast(<ToastUndo undo={undo} message={`Deleting ${person.name}`}/>, {
@@ -95,7 +137,8 @@ const PeopleTable = ({title, peopleList, URL_endpoint, setStateKey, updatePeople
   //////////////////////////// render ////////////////////////////
   return (
     <section>
-<h1 className="text-centered">{title}</h1>
+      <h1 className="text-centered">{title}</h1>
+      
       {showAll(peopleList, URL_endpoint)}
     </section>
   );
