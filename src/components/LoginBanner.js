09b16fa00c0d7f916155f5fbfe1c36b5876371d2
stupendousC <CarolineWuKaplan@gmail.com>
2020-01-19 02:42:40 -0800

    modal pop up can now pass on uuid to backend

--- src/components/LoginBanner.js
+++ src/components/LoginBanner.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import bannerLogo from '../images/SPPbanner2.png';
 import GoogleLogin from 'react-google-login';
+import Modal from 'react-bootstrap/Modal';
+import axios from 'axios';
 
 import {Link} from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const LoginBanner = ({authenticatedRole, googleAuthCallback, logoutCallback}) => {
 
+  const [uuid, setUuid] = useState("");
+
+  const [showModal, setShowModal] = useState(false);
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
   ////////////////////// LOGIN //////////////////////
   const responseGoogle = (response) => {
     // send info up to App.js     
@@ -25,6 +33,27 @@ const LoginBanner = ({authenticatedRole, googleAuthCallback, logoutCallback}) =>
         />
       </section>
     );
+  }
+  
+  const updateUuid = (e) => {
+    setUuid(e.target.value);
+  }
+
+  const sendUuidApi = (e) => {
+    e.preventDefault();
+    handleCloseModal();
+    console.log("SEND API WITH uuid =", uuid);
+    console.log(sessionStorage.getItem('googleId'));
+
+    const URL_endpoint = `${process.env.REACT_APP_LOGIN}/${sessionStorage.getItem('googleId')}`;
+
+    axios.post(URL_endpoint, {uuid: uuid})
+    .then(response => console.log(response.data))
+    .catch(error => toast.error(error.message));
+
+
+
+    
   }
 
   ////////////////////// DASHBOARD BUTTONS //////////////////////
@@ -48,7 +77,7 @@ const LoginBanner = ({authenticatedRole, googleAuthCallback, logoutCallback}) =>
     } else {
       return (
         <section className="dashboard-buttons_container">
-          <button className="btn btn-warning dashboard-buttons">First time logging in?  Click to activate account with UUID (upcoming feature)</button>
+          <button className="btn btn-warning dashboard-buttons" onClick={handleShowModal}>First time logging in?  Click to activate account!</button>
           <button className="btn btn-danger dashboard-buttons" onClick={logoutCallback}><Link to="/">LOGOUT</Link></button>
         </section>
       );
@@ -57,12 +86,29 @@ const LoginBanner = ({authenticatedRole, googleAuthCallback, logoutCallback}) =>
 
 
 ////////////////////// RENDER //////////////////////
-  // const currUrl = window.location.href;
-
   return(
-    <section className="loginBanner-section text-centered">
-      <Link to="/"><img src={bannerLogo} alt="sppBannerLogo" className="img-90"/></Link>
-      {authenticatedRole? showDashWithLogout():showGoogleLogin()}
+    <section>
+      <section className="loginBanner-section text-centered">
+        <Link to="/"><img src={bannerLogo} alt="sppBannerLogo" className="img-90"/></Link>
+        {authenticatedRole? showDashWithLogout():showGoogleLogin()}
+      </section>
+
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Welcome to Schedule Plus Plus!</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          Please copy and paste your unique verification id, including the dashes.
+          <form>
+            <input type="text" className="form-control margin-top-1rem" onChange={updateUuid} placeholder={"Example: 12345678-abcd-abcd-abcd-1234abcd1234"}/>
+            <section className="centered-children-per-row_container">
+              <button className="btn btn-primary margin-top-1rem" onClick={sendUuidApi}>Log in!</button>
+            </section>
+          </form>
+        </Modal.Body>
+      </Modal>
     </section>
   );
   
