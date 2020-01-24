4a6fc9f0429c1619c290f9523336228516c50b5a
stupendousC <CarolineWuKaplan@gmail.com>
2020-01-23 21:30:25 -0800

    made note on googleAuth weirdness to fix in future

--- src/components/LoginBanner.js
+++ src/components/LoginBanner.js
@@ -22,13 +22,11 @@ const LoginBanner = ({authenticatedRole, googleAuthCallback, logoutCallback}) =>
   const responseGoogle = (response) => {
     // send info up to App.js     
     const googleId = response.profileObj.googleId;
-    // const googleAccessToken = response.Zi.access_token;
-    
-    console.log(googleId);
     sessionStorage.setItem('googleId', googleId);
+
+    // TODO: FIX google Auth access token weirdness, had to comment out otherwise will break.  Never messing with deployment on day of presentation ever again...
+    // const googleAccessToken = response.Zi.access_token;
     // sessionStorage.setItem('googleAccessToken', googleAccessToken);
-
-
 
     googleAuthCallback();
   }
